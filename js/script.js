024f4842d392a6ef9c4bf2e32341d7eb83eaa0e9
Tom Maniglier <tom.maniglier@gmail.com>
2022-11-23 16:27:08 +0100

    v2

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const titleInput = document.querySelector('.title');
 const authorInput = document.querySelector('.author');
 const pagesInput = document.querySelector('.pages');
 const readInput = document.querySelector('.read');
+const container = document.querySelector('.container');
 
 function Book(title, author, pages, read) {
     this.title = title
@@ -15,8 +16,18 @@ function Book(title, author, pages, read) {
 }
 
 form.addEventListener('submit', () => {
-    const count = new Book(titleInput.value, authorInput.value, pagesInput.value, readInput.value);
-    count.info();
+    const phr = new Book(titleInput.value, authorInput.value, pagesInput.value, readInput.value);
+
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.innerHTML = `<span>${phr.title}</span><span>${phr.author}</span><span>${phr.read} / ${phr.pages}</span>`;
+    
+    container.appendChild(card);
+
+    card.addEventListener('click', () => {
+        card.remove();
+    })
+
     titleInput.value = '';
     authorInput.value = '';
     pagesInput.value = '';
